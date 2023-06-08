3612222c6cad64a827bcbc377e443ae08a218d9a
Pavel <b27394@gmail.com>
2023-06-08 23:48:05 +0300

    start profile

--- src/utils/templator.js
+++ src/utils/templator.js
@@ -1,14 +1,16 @@
 import Handlebars from 'handlebars';
 
-import form from '../components/templates/form.tmpl';
 import errorPage from '../components/templates/errorPage.tmpl'
+import profilePage from '../components/templates/profile.tmpl'
+import formField from '../components/partials/formField';
+import form from '../components/templates/form.tmpl';
 import button from '../components/partials/button';
 import link from '../components/partials/link'
-import formField from '../components/partials/formField';
 
+Handlebars.registerPartial('formField', formField);
 Handlebars.registerPartial('button', button);
 Handlebars.registerPartial('link', link);
-Handlebars.registerPartial('formField', formField);
 
-export const formTemplate = Handlebars.compile(form);
+export const profileTemplate = Handlebars.compile(profilePage);
 export const errorTemplate = Handlebars.compile(errorPage);
+export const formTemplate = Handlebars.compile(form);
