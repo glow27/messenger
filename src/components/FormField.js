3a50d47f033138d3fd44c92c641bde7a9d5b1009
Pavel <b27394@gmail.com>
2023-06-09 19:25:33 +0300

    fix node version

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,7 +1,7 @@
 import Handlebars from 'handlebars';
 
 export const FormField = ({ fieldName, fieldLabel }) =>
-  Handlebars.compile(`<label for={{ fieldName }} >{{ fieldLabel }}</label>
-  <input type="text"  name={{ fieldName }} id={{ fieldName }} class="formField" />`)(
-    { fieldName, fieldLabel }
-  );
+  Handlebars.compile(`
+    <label for={{ fieldName }} >{{ fieldLabel }}</label>
+    <input type="text"  name={{ fieldName }} id={{ fieldName }} class="formField" />`
+  )({ fieldName, fieldLabel });
